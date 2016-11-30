4c1528e462a36f07eafc7c4b0d48e85f2735bb80
k4ndAr3c <k4ndAr3c@gmx.com>
2016-11-30 16:36:43 +0100

    randomSongs

--- randomSongs.py
+++ randomSongs.py
@@ -1,25 +1,36 @@
 # -*- coding:Utf-8 -*-
-#!/usr/bin/env python2.7
-#auteur:k4ndAr3c
+#!/usr/bin/env python2
+#auteur:K4ndAr3c
 
 import fnmatch, shutil
-import os, random, sys
- 
-rootPath = str(sys.argv[1])
-pattern = '*.mp3'
-i=1   
-l=[]
+import os, random, sys, re
+from argparse import ArgumentParser
 
-for root, dirs, files in os.walk(rootPath):
-	for filename in fnmatch.filter(files, pattern):
-		l.append(os.path.join(root, filename))
+parser = ArgumentParser(prog='Myrandom.py', usage='Myrandom.py -i PathToMusicDir -o PathToOutDir -n HowMuch')
+parser.add_argument('-i', "--indir", type=str, help='Path to music folder')
+parser.add_argument('-o', "--outdir", type=str, help='Path to usb')
+parser.add_argument('-n', "--num",  type=int, help='How much songs to copy')
+args = parser.parse_args()
 
-lo = len(l)
+print len(sys.argv)
+if args.indir and args.outdir and args.num:
+	rootPath = args.indir
+	pattern = re.compile(r'.+\.(mp3)$', re.IGNORECASE)
+	i=1   
+	l=[]
+
+	for root, dirs, files in os.walk(rootPath):
+		l.extend(os.path.join(root, name) for name in files if pattern.match(name))
+		#for filename in fnmatch.filter(files, pattern):
+			#l.append(os.path.join(root, filename))
+
+	lo = len(l)
 		
-while i<int(sys.argv[3]):														
-	b = random.randint(1, lo)	
-	shutil.copy2(l[b],sys.argv[2])
-        print "%i  Copying %s" % (i, l[b])
-	i=i+1	
-	
+	while i<args.num:														
+		b = random.randint(1, lo)	
+		shutil.copy2(l[b],args.outdir)
+	        print "%i> %s" % (i, l[b])
+		i=i+1	
+else:
+	parser.print_help()
 
